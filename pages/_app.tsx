98fadaf053f821f2830c2e8ca5452a922d6ccd55
order&chaos <sean@orderandchaoscreative.com>
2022-12-02 22:40:03 +0000

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {UserProvider} from '@auth0/nextjs-auth0';
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
-        <UserProvider>
-            <Component {...pageProps} />
-        </UserProvider>
+        <>
+            <Head>
+                <title>Sommelier AI — Order &amp; Chaos</title>
+                <meta
+                    name="description"
+                    content="Generate wine tasting notes &amp; find recommendations"
+                />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#39393D"/>
+                <meta name="msapplication-TileColor" content="#39393d"/>
+                <meta name="theme-color" content="#39393d"/>
+            </Head>
+            <UserProvider>
+                <Component {...pageProps} />
+            </UserProvider>
+        </>
     )
 }
 
